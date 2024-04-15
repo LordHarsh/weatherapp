2c53fc7f0a1ce912e8c0dfbd1fd938c242a72ca6
LordHarsh <harshhbanka321@gmail.com>
2024-04-16 00:11:27 +0530

    feat: current weather component

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React from "react";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import CurrentWeather from "./src/components/CurrentWeather";
+const app = () => {
   return (
-    <View style={{height: 100, width: 100, backgroundColor: 'red'}}>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <CurrentWeather />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container:{
+    flex: 1
+  }
 });
+
+export default app;
